e41a3a2b34fc00670e31f2c0f918849284775e4d
ivan-lopezn <ivan.lopezn@edu.uah.es>
2022-02-17 17:14:25 +0100

    Inicio avión arcade

---
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -0,0 +1,6 @@
+import arcade
+arcade.open_window(800,600,"Avión")
+arcade.set_background_color(arcade.color.BLUEBONNET)
+arcade.start_render()
+#Estructura del avión
+arcade.draw_ellipse_filled(400,300,400,300,arcade.color.AUROMETALSAURUS)
